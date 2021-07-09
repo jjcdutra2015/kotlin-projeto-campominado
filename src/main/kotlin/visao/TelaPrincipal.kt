4537de09e53c29a55f8ad251ae9ab865c1282579
Julio Dutra <jjcdutra@gmail.com>
2021-07-09 19:43:33 -0300

    Classe TelaPrincipal

---
+++ src/main/kotlin/visao/TelaPrincipal.kt
@@ -0,0 +1,41 @@
+package visao
+
+import modelo.Tabuleiro
+import modelo.TabuleiroEvento
+import javax.swing.JFrame
+import javax.swing.JOptionPane
+import javax.swing.SwingUtilities
+
+fun main() {
+    TelaPrincipal()
+}
+
+class TelaPrincipal : JFrame() {
+    val tabuleiro = Tabuleiro(qtdeLinhas = 16, qtdeColunas = 30, qtdeMinas = 89)
+    val painelTabuleiro = PainelTabuleiro(tabuleiro)
+
+    init {
+        tabuleiro.onEvento { this::mostrarResultado }
+        add(painelTabuleiro)
+
+        setSize(690, 438)
+        setLocationRelativeTo(null)
+        defaultCloseOperation = EXIT_ON_CLOSE
+        title = "Campo Minado"
+        isVisible = true
+    }
+
+    private fun mostrarResultado(evento: TabuleiroEvento) {
+        SwingUtilities.invokeLater {
+            val msg = when (evento) {
+                TabuleiroEvento.VITORIA -> "Você ganhou"
+                TabuleiroEvento.DERROTA -> "Você perdeu... :P"
+            }
+            JOptionPane.showMessageDialog(this, msg)
+            tabuleiro.reiniciar()
+
+            painelTabuleiro.repaint()
+            painelTabuleiro.validate()
+        }
+    }
+}
